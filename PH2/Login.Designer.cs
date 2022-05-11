73c476406056a4bb9c8f070ef993b332f1281c3b
DavidNyx <63409430+DavidNyx@users.noreply.github.com>
2022-05-11 18:24:22 +0700

    Update Login.Designer.cs

--- PH2/Login.Designer.cs
+++ PH2/Login.Designer.cs
@@ -80,6 +80,7 @@ namespace PH2
             this.password.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.password.Location = new System.Drawing.Point(277, 184);
             this.password.Name = "password";
+            this.password.PasswordChar = '*';
             this.password.Size = new System.Drawing.Size(318, 34);
             this.password.TabIndex = 4;
             // 
